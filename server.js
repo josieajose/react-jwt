5ba6b2d693d5219c00691a36eadb46147ab53ad3
Joshua <josieboy55@gmail.com>
2021-04-16 12:43:49 +0300

    update

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + '/'));
-app.listen(process.env.PORT || 8080);
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'front_end', 'build')));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, 'front_end', 'build', 'index.html'))
+    });
+    
+  }
