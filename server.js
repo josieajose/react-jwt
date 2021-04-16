795b30e47bc79581b8b17f498828c10fdefae586
Joshua <josieboy55@gmail.com>
2021-04-16 12:22:06 +0300

    update

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-var connect = require("connect");
-
-var app = connect().use(connect.static(__dirname + '/public'));
-
-app.listen(8180);
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'));
+}
+app.get('*',(req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
