948ad85fa3c3862048094e1f206566ee3c47e9df
Joshua <josieboy55@gmail.com>
2021-04-16 12:25:16 +0300

    update

--- server.js
+++ server.js
@@ -1,7 +1,4 @@
-if(process.env.NODE_ENV === 'production'){
-    //set static folder
-    app.use(express.static('client/build'));
-}
-app.get('*',(req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
+var express = require('express');
+var app = express();
+app.use(express.static(client/build));
+app.listen(process.env.PORT || 8080);
