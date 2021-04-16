71aa658b6f3a81e8fabd7863232ac6b89a8f90d7
Joshua <josieboy55@gmail.com>
2021-04-16 12:18:21 +0300

    update

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-var express = require('express');
-var app = express();
-app.use(express.static(__dirname + '/'));
-app.listen(process.env.PORT || 8080);
+var connect = require("connect");
+
+var app = connect().use(connect.static(__dirname + '/public'));
+
+app.listen(8180);
